5bc41712a3ad74e34b6bc57c2764309af69488de
Nerdytoo <39653510+olalemi@users.noreply.github.com>
2022-10-15 11:38:31 +0100

    robo friends projects

---
+++ src/Card.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+
+
+const Card = () => {
+  return (
+
+            <div className='tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img alt ='robots' src ='https://robohash.org/test?200x200'/>
+
+            <div>
+                <h2> Nurudeen Olalemi</h2>
+                <p>olalemi.nurudeen@gmail.com</p>
+            </div>
+
+            </div>
+
+  )
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import Card from './Card'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+ReactDOM.render(
+         <div>
+             <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+             <Card/>
+             <Card/>
+             <Card/>
+
+         </div>
+        
+
+    ,document.getElementById('root'));
